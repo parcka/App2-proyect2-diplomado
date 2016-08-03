c8dfb1fcef717d7754ac4354ded974153bf3481e
parcka <rmanchon@hotmail.com>
2016-08-03 00:34:54 -0400

    Segundo proyecto Diplomado
    Proyecto de Carga de imagen y reproduccion de musica con menu de
    navegacion y notificaciones

---
+++ app/src/androidTest/java/com/parcka/xtr100/app2_proyect2_diplomado/ApplicationTest.java
@@ -0,0 +1,13 @@
+package com.parcka.xtr100.app2_proyect2_diplomado;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
---
+++ app/src/test/java/com/parcka/xtr100/app2_proyect2_diplomado/ExampleUnitTest.java
@@ -0,0 +1,15 @@
+package com.parcka.xtr100.app2_proyect2_diplomado;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * To work on unit tests, switch the Test Artifact in the Build Variants view.
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() throws Exception {
+        assertEquals(4, 2 + 2);
+    }
+}
